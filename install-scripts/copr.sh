1d9d1c85bb385375bda55f71013b9dea21cee715
Skivtp <118019606+Skivtp@users.noreply.github.com>
2025-09-03 01:24:34 +0300

    Update copr.sh

--- install-scripts/copr.sh
+++ install-scripts/copr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 💫 https://github.com/JaKooLit 💫 #
-# COPR repo and tweaking of dnf #
+# COPR repo and tweaking of rpm-ostree #
 
 # COPR Repos and packages needed from them
 # solopasha/hyprland - most packages
@@ -32,24 +32,16 @@ fi
 # Set the name of the log file
 LOG="Install-Logs/install-$(date +%d-%H%M%S)_copr.log"
 
-# Function to add dnf config if not present
-add_config_if_not_present() {
-  local file="$1"
-  local config="$2"
-  grep -qF "$config" "$file" || echo "$config" | sudo tee -a "$file" > /dev/null
-}
 
-# Check and add configuration settings to /etc/dnf/dnf.conf
-add_config_if_not_present "/etc/dnf/dnf.conf" "max_parallel_downloads=5"
-add_config_if_not_present "/etc/dnf/dnf.conf" "fastestmirror=True"
-add_config_if_not_present "/etc/dnf/dnf.conf" "defaultyes=True"
+
+
 
 # Enable RPM Fusion repositories
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
+sudo rpm-ostree install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
 
 # Enable COPR Repositories
 for repo in "${COPR_REPOS[@]}"; do
-  sudo dnf copr enable -y "$repo" 2>&1 | tee -a "$LOG" || { printf "%s - Failed to enable necessary copr repos\n" "${ERROR}"; exit 1; }
+  sudo rpm-ostree copr enable -y "$repo" 2>&1 | tee -a "$LOG" || { printf "%s - Failed to enable necessary copr repos\n" "${ERROR}"; exit 1; }
 done
 
 printf "\n%.0s" {1..1}
@@ -86,6 +78,6 @@ done
 printf "\n%.0s" {1..1}
 
 # Update package cache and install packages
-sudo dnf update -y
+sudo rpm-ostree update -y
 
 printf "\n%.0s" {1..2}
